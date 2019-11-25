5981aab4a95e714f9362204f62d8bba4ee9e5048
Julia Clower <juliaclower@Julias-MacBook-Air.local>
2019-11-25 14:10:07 -0500

    the front end is loading

--- generator_project/generator_app/urls.py
+++ generator_project/generator_app/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register('drinks', views.DrinksView)
+router.register('dinner', views.DinnerView)
+router.register('event', views.EventView)
+
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
--- generator_project/generator_app/views.py
+++ generator_project/generator_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from .serializers import DrinksSerializer, DinnerSerializer, EventSerializer
+from .models import Drinks, Dinner, Event
+
+
+class DrinksView(viewsets.ModelViewSet):
+    queryset = Drinks.objects.all()
+    serializer_class = DrinksSerializer
+
+class DinnerView(viewsets.ModelViewSet):
+    queryset = Dinner.objects.all()
+    serializer_class = DinnerSerializer
+
+class EventView(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
