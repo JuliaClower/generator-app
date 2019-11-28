3f4867ed776fb58c7dc5c73c56ce58cdd1f79a1e
Julia Clower <juliaclower@Julias-MacBook-Air.local>
2019-11-27 21:14:36 -0500

    final date is showing names of drink, dinner, event

--- client/src/components/Wizard.js
+++ client/src/components/Wizard.js
@@ -7,109 +7,112 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default class Wizard extends Component {
-    state = {
-        event: {},
-        dinner: {},
-        selectedDrink: {},
-        selectedDinner: {},
-        selectedEvent: {},
-        noDrinksSelected: true,
-        noDinnerSelected: true,
-        noEventSelected: true,
-        drinksList: [],
-        eventList: [],
-        dinnerList: []
-    }
-    componentDidMount = () => {
-        this.getAllDrinks()
-    }
-    getAllDrinks = () => {
-        axios.get('api/v1/drinks')
-            .then((res) => {
-                const drinksList = res.data;
-                this.setState({ drinksList: drinksList })
-            })
-    }
-    getAllDinner = () => {
-        axios.get('api/v1/dinner')
-            .then((res) => {
-                const dinnerList = res.data;
-                this.setState({ dinnerList: dinnerList })
-            });
-    };
-    getAllEvent = () => {
-        axios.get('api/v1/event')
-            .then((res) => {
-                const eventList = res.data;
-                this.setState({ eventList: eventList })
-            });
-    };
-    drinksSelectClick = (drink) => {
-        console.log('drinks clicked')
-        console.log(drink)
-        this.setState({ selectedDrink: drink })
-        this.setState({ noDrinksSelected: false })
-        this.getAllDinner()
-    }
-    dinnerSelectClick = (dinner) => {
-        this.setState({ selectedDinner: dinner })
-        this.setState({ noDinnerSelected: false })
-        this.getAllEvent()
-    }
-    eventSelectClick = (event) => {
-        this.setState({ selectedEvent: event})
-        this.setState({ noEventSelected: false })
-    }
-    render() {
-        let drinksList = []
-        let dinnerList = []
-        let eventList = []
-        if (this.state.noDrinksSelected) {
-            drinksList = this.state.drinksList.map((drink) => {
-                return (
-                    <button
-                        onClick={() => { this.drinksSelectClick(drink) }}
-                    >
-                        {drink.name}
-                    </button>
-                )
-            })
-        } 
-        if (this.state.noDinnerSelected) {
-            dinnerList = this.state.dinnerList.map((dinner) => {
-                return (
-                    <button
-                        onClick={() => { this.dinnerSelectClick(dinner) }}
-                    >
-                        {dinner.name}
-                    </button>
-                )
-            })
-        }
-        if (this.state.noEventSelected) {
-            eventList = this.state.eventList.map((event) => {
-                return (
-                    <button 
-                        onClick={() => { this.eventSelectClick(event) }}
-                    >
-                        {event.name}
-                    </button>
-                )
-            })
-        } else {
-            console.log('this.state.noDrinksSelected', this.state.noDrinksSelected)
-            return (
+	state = {
+		event: {},
+		dinner: {},
+		selectedDrink: {},
+		selectedDinner: {},
+		selectedEvent: {},
+		noDrinksSelected: true,
+		noDinnerSelected: true,
+		noEventSelected: true,
+		drinksList: [],
+		eventList: [],
+		dinnerList: []
+	}
+	componentDidMount = () => {
+		this.getAllDrinks()
+	}
+	getAllDrinks = () => {
+		axios.get('api/v1/drinks')
+			.then((res) => {
+				const drinksList = res.data;
+				this.setState({ drinksList: drinksList })
+			})
+	}
+	getAllDinner = () => {
+		axios.get('api/v1/dinner')
+			.then((res) => {
+				const dinnerList = res.data;
+				this.setState({ dinnerList: dinnerList })
+			});
+	};
+	getAllEvent = () => {
+		axios.get('api/v1/event')
+			.then((res) => {
+				const eventList = res.data;
+				this.setState({ eventList: eventList })
+			});
+	};
+	drinksSelectClick = (drink) => {
+		console.log('drinks clicked')
+		console.log(drink)
+		this.setState({ selectedDrink: drink })
+		this.setState({ noDrinksSelected: false })
+		this.getAllDinner()
+	}
+	dinnerSelectClick = (dinner) => {
+		this.setState({ selectedDinner: dinner })
+		this.setState({ noDinnerSelected: false })
+		this.getAllEvent()
+	}
+	eventSelectClick = (event) => {
+		this.setState({ selectedEvent: event })
+		this.setState({ noEventSelected: false })
+	}
+	render() {
+		let drinksList = []
+		let dinnerList = []
+		let eventList = []
+		if (this.state.noDrinksSelected) {
+			drinksList = this.state.drinksList.map((drink) => {
+				return (
+					<button
+						onClick={() => { this.drinksSelectClick(drink) }}
+					>
+						{drink.name}
+					</button>
+				)
+			})
+		}
+		if (this.state.noDinnerSelected) {
+			dinnerList = this.state.dinnerList.map((dinner) => {
+				return (
+					<button
+						onClick={() => { this.dinnerSelectClick(dinner) }}
+					>
+						{dinner.name}
+					</button>
+				)
+			})
+		}
+		if (this.state.noEventSelected) {
+			eventList = this.state.eventList.map((event) => {
+				return (
+					<button
+						onClick={() => { this.eventSelectClick(event) }}
+					>
+						{event.name}
+					</button>
+				)
+			})
+		} else {
+			console.log('this.state.noDrinksSelected', this.state.noDrinksSelected)
+			return (
+				<div>
+					<p>{this.state.selectedDrink.name}</p>
+					<p>{this.state.selectedDinner.name}</p>
+					<p>{this.state.selectedEvent.name}</p>
+				</div>
 
-                <p>{this.state.noDrinksSelected === false ? this.state.selectedDrink.name : 'no drink selected'}</p>
-
-            )
-        }
-        return (
-            <div>
-                {drinksList}
-                {dinnerList}
-                {eventList}
-            </div>
-        )
-    }
+			)
+		}
+		return (
+			<div>
+				{drinksList}
+				{dinnerList}
+				{eventList}
+			</div>
+		)
+	}
 }
